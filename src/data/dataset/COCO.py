db3f45a4cbc8f03823851dc77d51a483f877285e
MaxLievense <max1lievense@gmail.com>
2024-10-08 20:06:28 +0200

    Adding `min_area` config to filter on Bbox size

--- src/data/dataset/COCO.py
+++ src/data/dataset/COCO.py
@@ -14,7 +14,7 @@ logging.getLogger("PIL").setLevel(logging.WARNING)
 class COCODataset(Dataset):
     """COCO Custom Dataset compatible with torch.utils.data.DataLoader."""
 
-    def __init__(self, root, json, transform=None):
+    def __init__(self, root, json, transform=None, min_area=None):
         """Set the path for images and captions.
 
         Args:
@@ -27,6 +27,14 @@ class COCODataset(Dataset):
         self.ids = list(self.coco.imgs.keys())
         self.transform = transform
         self.n_classes = len(self.coco.cats)
+        self.min_area = min_area
+
+        if min_area is not None:
+            self.ids = [
+                img_id
+                for img_id in self.ids
+                if any([ann["area"] >= min_area for ann in self.coco.loadAnns(self.coco.getAnnIds(imgIds=img_id))])
+            ]
 
     def __getitem__(self, index):
         raise NotImplementedError
@@ -35,36 +43,36 @@ class COCODataset(Dataset):
         return len(self.ids)
 
 
-class COCOSinglebbox(COCODataset):
-    """COCO Custom Dataset compatible with torch.utils.data.DataLoader."""
+# class COCOSinglebbox(COCODataset):
+#     """COCO Custom Dataset compatible with torch.utils.data.DataLoader."""
 
-    def __init__(self, root, json, transform=None):
-        """Set the path for images and captions.
+#     def __init__(self, root, json, transform=None):
+#         """Set the path for images and captions.
 
-        Args:
-            root: image directory.
-            json: coco annotation file path.
-            transform: image transformer.
-        """
-        self.root = root
-        self.coco = COCO(json)
-        self.ids = list(self.coco.anns.keys())
-        self.transform = transform
-        self.n_classes = len(self.coco.cats)
+#         Args:
+#             root: image directory.
+#             json: coco annotation file path.
+#             transform: image transformer.
+#         """
+#         self.root = root
+#         self.coco = COCO(json)
+#         self.ids = list(self.coco.anns.keys())
+#         self.transform = transform
+#         self.n_classes = len(self.coco.cats)
 
-    def __getitem__(self, index):
-        """Returns one data pair (image and bbox)."""
-        ann_id = self.ids[index]
-        bbox = self.coco.anns[ann_id]["bbox"]
-        img_id = self.coco.anns[ann_id]["image_id"]
-        path = self.coco.loadImgs(img_id)[0]["file_name"]
+#     def __getitem__(self, index):
+#         """Returns one data pair (image and bbox)."""
+#         ann_id = self.ids[index]
+#         bbox = self.coco.anns[ann_id]["bbox"]
+#         img_id = self.coco.anns[ann_id]["image_id"]
+#         path = self.coco.loadImgs(img_id)[0]["file_name"]
 
-        image = Image.open(os.path.join(self.root, path)).convert("RGB")
-        if self.transform is not None:
-            image = self.transform(image)
+#         image = Image.open(os.path.join(self.root, path)).convert("RGB")
+#         if self.transform is not None:
+#             image = self.transform(image)
 
-        target = torch.Tensor(bbox)
-        return image, target
+#         target = torch.Tensor(bbox)
+#         return image, target
 
 
 class COCOMultibbox(COCODataset):
@@ -76,6 +84,9 @@ class COCOMultibbox(COCODataset):
         img_id = self.ids[index]
         ann_ids = coco.getAnnIds(imgIds=img_id)
         anns = coco.loadAnns(ann_ids)
+
+        if self.min_area is not None:
+            anns = [ann for ann in anns if ann["area"] >= self.min_area]
 
         path = coco.loadImgs(img_id)[0]["file_name"]
         image = Image.open(os.path.join(self.root, path)).convert("RGB")
