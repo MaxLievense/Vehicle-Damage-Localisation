b47e364db99a495171075b7bfdde1c4a99f1c318
MaxLievense <max1lievense@gmail.com>
2024-10-15 10:58:43 +0200

    Refactored and documented module `model`.

--- src/model/detection/RCNN/FasterRCNN.py
+++ src/model/detection/RCNN/FasterRCNN.py
@@ -1,31 +1,62 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 import torch
 import torchvision
 
 from src.model.model import BaseModel
 
+if TYPE_CHECKING:
+    from src.data.data import Data
+    from src.trainer.trainer import BaseTrainer as Trainer
+
 
 class FasterRCNN(BaseModel):
-    def _init_modules(self):
-        super()._init_modules()
-        in_features = self.network.roi_heads.box_predictor.cls_score.in_features
-        self.network.roi_heads.box_predictor = torchvision.models.detection.faster_rcnn.FastRCNNPredictor(
-            in_features, self.n_classes + 1
-        )  # add one for background
-        self.network.to(self.device)
+    """
+    Interface for Faster R-CNN model.
+    """
 
-    def step(self, trainer, data, target):
+    def init_network(self) -> torch.nn.Module:
+        """
+        Fixes the number of classes in the FastRCNNPredictor.
+
+        Returns:
+            torch.nn.Module: The Faster R-CNN model.
+        """
+        network = super().init_network()
+        in_features = network.roi_heads.box_predictor.cls_score.in_features
+        network.roi_heads.box_predictor = torchvision.models.detection.faster_rcnn.FastRCNNPredictor(
+            in_features, self.n_classes + 1
+        ).to(self.device)
+        return network
+
+    def step(
+        self, trainer: Trainer, data: Data, target: dict[str, torch.Tensor]
+    ) -> tuple[float, dict[str, torch.Tensor]]:
+        """
+        Custom step function for Faster R-CNN.
+
+        Args:
+            trainer (Trainer): Trainer object.
+            data (Data): Data object.
+            target (dict[str, torch.Tensor]): Target dictionary.
+
+        Returns:
+            tuple[float, dict[str, torch.Tensor]]: Loss and output dictionary.
+        """
         if self.network.training:
             with torch.set_grad_enabled(True):
                 # TODO: The Dataloader returns tensors on CPU, trainer only sends to device if tensor, not dict.
                 for i, _target in enumerate(target):
                     for k, v in _target.items():
                         target[i][k] = v.to(self.device)
-                output = self.network(data, target)
+                output = self.network.forward(data, target)
                 loss = sum(list(output.values()))
                 loss.backward()
 
                 if self.cfg.clip_grad:
                     torch.nn.utils.clip_grad_norm_(self.network.parameters(), max_norm=self.cfg.clip_grad)
                 return loss.item(), output
-        output = self.network(data)
+        output = self.network.forward(data)
         return None, output
--- src/model/model.py
+++ src/model/model.py
@@ -1,65 +1,125 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 import torch
 from hydra.utils import instantiate
-from omegaconf import DictConfig
 
 from src.utils.base import Base
 
 if TYPE_CHECKING:
     from src.data.data import Data
+    from src.trainer.trainer import BaseTrainer as Trainer
 
 
 class BaseModel(torch.nn.Module, Base):
-    """Abstract Model class. All models should inherit from this class."""
+    """
+    Abstract Model class that handles model creation and specific model steps.
+    All models should inherit from this class and implement the necessary methods.
+    """
 
-    def __init__(
-        self,
-        device: torch.device,
-        data: Data,
-        **cfg: dict,
-    ) -> None:
+    def __init__(self, device: torch.device, data: Data, **cfg: dict):
+        """
+        Initializes the BaseModel with the device and data, and sets up the model architecture.
+
+        Args:
+            device (torch.device): Device on which the model will be trained.
+            data (Data): Data object.
+            **cfg (dict): Remaining Hydra configurations.
+        """
         super().__init__()
         Base.__init__(self, cfg)
 
-        self.log.debug("Building model...")
         self.device = device
         self.data = data
         self.n_classes = data.n_classes
-        self._init_modules()
-        self.log.info(f"Loaded model:\n{self.network}")
 
-    def _init_modules(self):
-        self.network = instantiate(DictConfig(self.cfg.network), n_classes=self.n_classes, device=self.device)
-        self.network.to(self.device)
+        self.network = self.init_network()
+        self.log.debug(f"Loaded model:\n{self.network}")
+        self.criterion = self.init_criterion()
+        self.log.debug(f"Loaded criterion:\n{self.criterion}")
 
-        if self.cfg.get("criterion"):
-            self.criterion = instantiate(DictConfig(self.cfg.criterion))
-            self.criterion.to(self.device)
-            self.log.info(f"Loaded criterion:\n{self.criterion}")
-        else:
-            self.criterion = None
+    def init_network(self) -> torch.nn.Module:
+        """
+        Initializes the network architecture.
+
+        Returns:
+            torch.nn.Module: The network model.
+        """
+        network = instantiate(self.cfg.network, n_classes=self.n_classes, device=self.device)
+        network.to(self.device)
+        return network
+
+    def init_criterion(self) -> torch.nn.Module:
+        """
+        Initializes the loss criterion, if defined in the configuration.
+
+        Returns:
+            torch.nn.Module: The loss criterion.
+        """
+        if not self.cfg.get("criterion"):
             self.log.warning("No criterion loaded. Assuming criterion is defined in network.")
+            return None
+        criterion = instantiate(self.cfg.criterion)
+        criterion.to(self.device)
+        return criterion
 
-    def __call__(self, x):
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Overrides the call method to directly forward input through the network.
+
+        Args:
+            x (torch.Tensor): Input tensor to the model.
+
+        Returns:
+            torch.Tensor: Output from the forward pass of the network.
+        """
         return self.forward(x)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Defines the forward pass of the network.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Output tensor after processing through the network.
+        """
         return self.network(x)
 
-    def step(self, trainer, data, target):
-        raise NotImplementedError
+    def step(self, trainer: Trainer, data: torch.Tensor, target: torch.Tensor) -> Any:
+        """
+        Perform a single training step, to be implemented by the inherited model.
 
-    def pretrain(self, *args, **kwargs):
-        pass
+        Args:
+            trainer (Trainer): The trainer object managing the training process.
+            data (torch.Tensor): Input data for the step.
+            target (torch.Tensor): Target labels or values for the step.
 
-    def posttrain(self, *args, **kwargs):
-        pass
+        Raises:
+            NotImplementedError: This method must be implemented by any subclass.
+        """
+        raise NotImplementedError("Subclasses must implement this method.")
 
-    def preepoch(self, *args, **kwargs):
+    def pretrain(self, *args: Any, **kwargs: Any) -> None:
+        """
+        Optional method to be called before training starts. Can be overridden in subclasses.
+        """
+
+    def posttrain(self, *args: Any, **kwargs: Any) -> None:
+        """
+        Optional method to be called after training ends. Can be overridden in subclasses.
+        """
+
+    def preepoch(self, *args: Any, **kwargs: Any) -> None:
+        """
+        Method called before each training epoch to perform any necessary preparations.
+        This implementation clears the CUDA cache.
+        """
         torch.cuda.empty_cache()
 
-    def postepoch(self, *args, **kwargs):
-        pass
+    def postepoch(self, *args: Any, **kwargs: Any) -> None:
+        """
+        Optional method called after each training epoch. Can be overridden in subclasses.
+        """
